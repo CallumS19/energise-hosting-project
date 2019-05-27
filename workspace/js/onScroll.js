46f0034e5cbaaaba391421702723d8f68c0c6fb2
Callum Spiers <20201868@student.glasgowclyde.ac.uk>
2019-05-27 08:36:55 +0100

    Third Commit

--- workspace/js/onScroll.js
+++ workspace/js/onScroll.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
-	  if (scroll > 870) {
+	  if (scroll > 630) {
 	    $("nav").css("background" , "#b91e2a");
 	  }
 
 	  else{
 		  $("nav").css("background" , "transparent");  	
 	  }
-  })
+	})
 })
+/*$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	if (scroll > 500) {
+		$("nav").css("background" , "#b91e2a");
+	}
+
+	else{
+		$("nav").css("background" , "transparent");  	
+	}
+})
+
+$(window).resize(function () {
+	$(window).scroll(function(){
+	var scroll = $(window).scrollTop();
+	if ($(window).outerWidth() < 1000 && scroll > 1000) {
+		$("nav").css("background" , "#b91e2a");
+	} else {
+		$("nav").css("background" , "transparent");
+	}
+})
+});*/
